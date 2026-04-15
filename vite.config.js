48e0298fc23dacb0fb4be184a5a9791371364f6d
Assane Sarr <assanesarr030@gmail.com>
2026-04-15 21:38:30 +0000

    add github actions deploy

--- vite.config.js
+++ vite.config.js
@@ -16,5 +16,5 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  // base: '/transitPro-demo/' // IMPORTANT pour GitHub Pages
+  base: '/transitPro-demo/' // IMPORTANT pour GitHub Pages
 })
